053cca5cae968361d2ab417791e8a3ebe8c6dfd3
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2024-01-26 01:36:25 +0800

    home page working with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-const App = () => {
-  return (
-    <div>
-    Hello how are you
-    </div>
-  )
-}
 
-export default App
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+const App = () => {
+   return ( 
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+     </BrowserRouter>
+  );
+}
+export default App;
